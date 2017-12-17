22f5d18e8d43283aaee392e5aab20711abf7c662
Ari <amindell@terpmail.umd.edu>
2017-12-16 20:08:37 -0500

    added json serializing to question class, wrote preliminary question logging class, added reader for question files

--- multi-test/util/java/main/src/Question.java
+++ multi-test/util/java/main/src/Question.java
@@ -1,6 +1,9 @@
 import org.apache.commons.lang3.StringUtils;
 
+import com.google.gson.Gson;
+
 public final class Question {
+	private static final Gson SERIALIZER = new Gson();
 	private final String[] answers;
 	private final String question;
 	private final boolean oddOneOut;
@@ -33,6 +36,17 @@ public final class Question {
 	}
 
 	public String getLoggableString() {
-		return question + "," + String.join(",", answers);
+		return SERIALIZER.toJson(this);
+	}
+
+	public static Question parseQuestion(String loggedString) {
+		return SERIALIZER.fromJson(loggedString, Question.class);
+	}
+
+	public static void main(String[] args) {
+		Gson gson = new Gson();
+		String json = gson
+				.toJson(new Question("why is ari sick", new String[] { "he just is", "idk", "lemme suck it" }));
+		System.out.println(json);
 	}
 }
---
+++ multi-test/util/java/main/src/QuestionListReader.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class QuestionListReader extends BufferedReader {
+	String currentLine;
+
+	public QuestionListReader(String fileName) throws FileNotFoundException {
+		super(new FileReader(fileName));
+		try {
+			currentLine = readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public Question nextQuestion() {
+		String quest = currentLine;
+		try {
+			currentLine = readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return Question.parseQuestion(quest);
+	}
+
+	public boolean hasNext() {
+		return currentLine != null;
+	}
+
+}
